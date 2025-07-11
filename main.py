e8c5feaf596fac6ef5a69f4eee51aad4fe37372d
nonaminho <eng.pedrotti@gmail.com>
2025-07-11 07:48:52 -0300

    create a GUI, a player and draw it in the screen

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
 import pygame
+from constants import *
+from player import Player
 
 def main():
+    pygame.init()
     print("Starting Asteroids!")
+    #print(f"Screen width: {SCREEN_WIDTH}")
+    #print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH ,SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    while True:
+        for event in pygame.event.get(): #Usado para fechar o jogo
+            if event.type == pygame.QUIT:
+                return
+            
+        screen.fill((0,0,0)) #Preenche o fundo
+        player.draw(screen)
+        pygame.display.flip()   #Atualiza o display
+
+        #Limita em 60 FPS
+        dt = clock.tick(60)/1000
 
 
 if __name__ == "__main__":
